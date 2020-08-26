693bb047157c1860feb6b457a1e847990e46727a
Zsolt van den Már <zsolt@vandenmar.com>
2020-08-26 09:24:28 -0700

    ZSM - Add PurgeCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-less`,
+    `gatsby-plugin-postcss`,
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -44,7 +56,6 @@ module.exports = {
         siteUrl: targetAddress.href.slice(0, -1),
       }
     },
-    `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
