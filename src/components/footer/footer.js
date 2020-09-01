65fc447957358ea9e367205f721d4e35296234dc
Zsolt van den Már <zsolt@vandenmar.com>
2020-09-01 13:34:10 -0700

    ZSM - Footer start

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,14 +20,34 @@ const AnimatedContainer = posed.div({
 
 const Footer = () => (
   <AnimatedContainer>
-    <Link to="/">
-      <h4>Footer</h4>
-    </Link>
-    <footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
-    </footer>
+    <div className="w-full flex justify-center">
+      <div className="flex items-center justify-between w-full
+        pa-0 md:py-3 md:px-2 bg-white fixed z-10"
+        style={{ maxWidth: 1280 }}>
+        
+        {/* Footnotes */}
+        <div className="flex flex-col md:flex-row items-center justify-between w-full
+          text-boost-secondary-30 text-sm">
+          <div className="flex flex-row">
+            © {new Date().getFullYear()}
+            <span className="pl-1 pr-3">— Lawmatics</span>
+            <div className="et-social-icon et-social-facebook">
+              <a href="https://www.facebook.com/Lawmatics-156829925019246/" className="icon">
+                Facebook
+              </a>
+            </div>
+            <div className="et-social-icon et-social-twitter pl-2">
+              <a href="https://twitter.com/lawmatics" className="icon">
+                Twitter
+              </a>
+            </div>
+          </div>
+          <div className="italic">
+            Crafted with <span className="parrot"></span> in San Diego, CA
+          </div>
+        </div>
+      </div>
+    </div>
   </AnimatedContainer>
 );
 
