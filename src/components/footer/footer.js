625134206aa641ceed4ab283c0ff7a59875a8807
Zsolt van den Már <zsolt@vandenmar.com>
2020-09-01 15:53:24 -0700

    ZSM - Add footer elements

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import posed from 'react-pose';
 
+const Parrot = styled.span`
+  background-image: url(//boost-static-assets.s3.amazonaws.com/images/parrot.gif);
+  background-size: contain;
+  width: 16px;
+  height: 16px;
+  display: inline-block;
+  margin-left: 5px;
+  margin-right: 3px;
+`
 
 const AnimatedContainer = posed.div({
   enter: {
@@ -27,23 +37,27 @@ const Footer = () => (
         
         {/* Footnotes */}
         <div className="flex flex-col md:flex-row items-center justify-between w-full
-          text-boost-secondary-30 text-sm">
-          <div className="flex flex-row">
+          text-boost-secondary-30 text-xs">
+          <div className="flex flex-row items-center">
             © {new Date().getFullYear()}
             <span className="pl-1 pr-3">— Lawmatics</span>
-            <div className="et-social-icon et-social-facebook">
-              <a href="https://www.facebook.com/Lawmatics-156829925019246/" className="icon">
-                Facebook
-              </a>
-            </div>
-            <div className="et-social-icon et-social-twitter pl-2">
-              <a href="https://twitter.com/lawmatics" className="icon">
-                Twitter
-              </a>
-            </div>
+            <a
+              href="https://www.facebook.com/Lawmatics-156829925019246/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-boost-secondary-70 text-base">
+              <span className="eleganticon social_facebook" />
+            </a>
+            <a
+              href="https://twitter.com/lawmatics"
+              target="_blank"
+              rel="noreferrer"
+              className="text-boost-secondary-70 text-base pl-3">
+              <span className="eleganticon social_twitter" />
+            </a>
           </div>
           <div className="italic">
-            Crafted with <span className="parrot"></span> in San Diego, CA
+            Crafted with <Parrot /> in San Diego, CA
           </div>
         </div>
       </div>
