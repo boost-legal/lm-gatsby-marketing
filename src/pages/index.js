5a2bc748d12b9832b6be6c461f063d45e9a31dfa
Zsolt van den Már <zsolt@vandenmar.com>
2020-09-02 12:21:45 -0700

    ZSM - Add animation to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,29 @@
 import React from "react"
+import posed from 'react-pose';
 import SEO from "../components/seo"
 import Logo from "../assets/images/lawmatics-preloader.svg";
 
 
+const AnimStagger = posed.div({
+  enter: { staggerChildren: 100, delayChildren: 500 }
+});
+const Anim = posed.span({
+  init: { marginTop: 50 },
+  enter: { marginTop: 0, opacity: 1,
+    transition: {
+      default: { ease: 'easeOut', duration: 1000 }
+    }},
+  exit: { opacity: 0 },
+});
+
 const IndexPage = () => (
   <div className="w-full h-screen flex flex-col items-center justify-center text-center">
     <SEO title="Under Development"/>
-    <Logo />
-    <h1>Coming soon</h1>
-    <p className="text-boost-secondary-50">Currently under development.</p>
+    <AnimStagger className="flex flex-col items-center">
+      <Anim><Logo /></Anim>
+      <Anim><h1>Coming soon</h1></Anim>
+      <Anim><p className="text-boost-secondary-50">Currently under development.</p></Anim>
+    </AnimStagger>
   </div>
 )
 
