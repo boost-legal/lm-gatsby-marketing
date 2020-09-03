7633388e9a6fdd6992d4e80d17fa16b3cd138160
Roey Chasman <rchasman@gmail.com>
2020-09-03 00:57:34 -0700

    RDC - Install and configure eslint for atom and gatsby develop runner.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,18 @@ module.exports = {
         siteUrl: targetAddress.href.slice(0, -1),
       }
     },
-    'gatsby-plugin-react-svg',
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
