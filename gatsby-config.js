9a9ba70ad7384e5dc98c7863049b992bd3e35d9e
Zsolt van den Már <zsolt@vandenmar.com>
2020-12-30 12:50:14 -0800

    ZSM - Add purge css exception for sal.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         stages: ['develop'],
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/sal.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
