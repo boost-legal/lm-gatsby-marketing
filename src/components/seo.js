f45f729842849c699b5567eec885494aeca5297c
Roey Chasman <rchasman@gmail.com>
2020-12-31 16:36:28 -0800

    RDC - Cleanup some seo code.

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types"
 import { GatsbySeo } from "gatsby-plugin-next-seo"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, title, customTitle }) {
-  const { site } = useStaticQuery(
+function SEO({ description, title, customTitle }) {
+  const { site: { siteMetadata } = useStaticQuery(
     graphql`
       query {
         site {
@@ -19,40 +19,38 @@ function SEO({ description, lang, title, customTitle }) {
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || siteMetadata.description
 
   return (
     <GatsbySeo
       language='en'
       title={title}
-      titleTemplate={customTitle || `${site.siteMetadata.title} - %s`}
+      titleTemplate={customTitle || `${siteMetadata.title} - %s`}
       description={metaDescription}
-      canonical={site.siteMetadata.siteUrl}
+      canonical={siteMetadata.siteUrl}
       twitter={{
         cardType: 'summary',
-        handle: site.siteMetadata.author,
-        site: site.siteMetadata.title,
+        handle: siteMetadata.author,
+        site: siteMetadata.title,
       }}
       openGraph={{
         title: customTitle || title,
         description: metaDescription,
         type: 'website',
-        url: site.siteMetadata.siteUrl,
-        site_name: site.siteMetadata.title,
+        url: siteMetadata.siteUrl,
+        site_name: siteMetadata.title,
       }}
     />
   )
 }
 
 SEO.defaultProps = {
-  lang: 'en',
   description: '',
   customTitle: null,
 }
 
 SEO.propTypes = {
   description: PropTypes.string,
-  lang: PropTypes.string,
   title: PropTypes.string.isRequired,
   customTitle: PropTypes.string,
 }
